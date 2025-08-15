6967252b0c39335099ea6201a94e0b2f690fba74
usmanmobeen111 <usmanmobeen111@gmail.com>
2025-08-15 21:36:15 +0500

    Cards and Animations

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const FeatureCard = ({ title, description, icon: Icon , iconClass, lineClass}) => {
   return (
-    <div>
+    <div className='bg-white/25 backdrop-blur-3xl md:w-[25vw] m-auto h-full p-10 rounded-2xl shadow-md hover:shadow-lg transition duration-300 flex flex-col items-center justify-items-center gap-5 hover:scale-[1.05]'>
       <Icon className={iconClass} />
       <div className={lineClass}></div>
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <h3 className='text-slate-900 font-bold text-2xl text-center'>{title}</h3>
+      <p className='text-center text-sm text-slate-600'>{description}</p>
     </div>
   )
 }
--- src/pages/Feature.jsx
+++ src/pages/Feature.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BackgroundShapes from '../components/BackgroundShapes'
 import { featureData } from '../data/featureData'
 import FeatureCard from '../components/FeatureCard'
+import {motion} from "framer-motion"
 
 const Feature = () => {
   return (
@@ -9,13 +10,16 @@ const Feature = () => {
       <div className="absolute -z-50">
         <BackgroundShapes/>
       </div>
-      <div>
-        <h2 className="text-4xl font-bold text-center">Feature Highlights</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
-          {featureData.map(feature => (
+      <div className='md:h-screen p-10 flex flex-col items-center justify-center'>
+        <motion.h2 initial={{opacity:0, y:10}} animate={{opacity:1, y:0}} transition={{duration:0.5}} className="text-4xl font-bold text-center">Feature Highlights</motion.h2>
+        <motion.div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8"
+        initial={{opacity:0, y:20}} animate={{opacity:1, y:0}} transition={{duration:0.5}}>
+        
+            {featureData.map(feature => (
             <FeatureCard key={feature.id} {...feature} />
           ))}
-        </div>
+         
+        </motion.div>
       </div>
     </div>
   )
